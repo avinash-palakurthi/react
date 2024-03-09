02d770dc7f78f73dfa3b7158fedd9d671a07d428
Avinash <palakurthiavinash07@gmail.com>
2024-03-09 11:33:18 +0530

    useState, counter proj

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	let [counter, setCounter] = useState(21);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const addValue = () => {
+		counter = counter + 1;
+		setCounter(counter);
+	};
+	const removeValue = () => {
+		counter = counter - 1;
+		setCounter(counter);
+	};
+	return (
+		<>
+			<main className="flex flex-col items-center justify-center">
+				<div className="bg-slate-600 w-full flex items-center justify-center text-white text-2xl p-1 ">
+					<h1>Counter App</h1>
+				</div>
+				<section className="flex flex-col items-center justify-center mt-20">
+					<div className="text-2xl">
+						<h1>count:{counter}</h1>
+					</div>
+					<div className="space-x-10 mt-5">
+						<button
+							onClick={addValue}
+							className="bg-slate-500 text-white p-2 px-4 rounded"
+						>
+							Increment
+						</button>
+						<button
+							onClick={removeValue}
+							className="bg-slate-500 text-white p-2 px-4 rounded"
+						>
+							Decrement
+						</button>
+					</div>
+				</section>
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
